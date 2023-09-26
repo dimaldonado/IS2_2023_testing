d88d232326fb5e4b4fe14e6f81c6d474451ad8bd
cvenegasa <cvenegasa@udec.cl>
2023-09-26 15:55:41 -0300

    feat: new unit test for login user verification

---
+++ is2_backend/test/test_login_user.py
@@ -0,0 +1,24 @@
+
+import pytest
+from ..lib.api.dbAPI import app
+
+@pytest.fixture
+def client():
+    with app.test_client() as client:
+        yield client
+
+def test_login(client):
+
+    test_data = {
+        "email": "test@example.com",
+        "password": "password123",
+    }
+
+    response = client.post('/login', json=test_data)
+
+
+    data = json.loads(response.data)
+
+    assert response.status_code == 200  
+    assert data["name"] == "Test User"  
+    assert data["email"] == "test@example.com"
