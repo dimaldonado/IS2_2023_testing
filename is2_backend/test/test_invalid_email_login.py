4085cb66bc8ed4810cd3e09c9ccf80783030d930
cvenegasa <cvenegasa@udec.cl>
2023-09-26 16:10:26 -0300

    feat: added unit tests for invalid email and pwd

---
+++ is2_backend/test/test_invalid_email_login.py
@@ -0,0 +1,25 @@
+
+import pytest
+from ..lib.api.dbAPI import app
+
+#crear un cliente simulado, para realizar solicitudes HTTP a la app flask
+@pytest.fixture
+def client():
+    with app.test_client() as client:
+        yield client
+
+def test_invalid_email_login(client):
+    # se define un email no existente para testear el mensaje de error
+    test_data = {
+        "email": "nonexistent@example.com",
+        "password": "password123",
+    }
+
+    # Send a POST request to the /login route
+    response = client.post('/login', json=test_data)
+
+    # Assertions for an invalid email
+    assert response.status_code == 401
+    data = json.loads(response.data)
+    assert "error" in data
+    assert data["error"] == "Correo o contraseña incorrectas"
---
+++ is2_backend/test/test_invalid_password_login.py
@@ -0,0 +1,35 @@
+
+import pytest
+from ..lib.api.dbAPI import app
+
+@pytest.fixture
+def client():
+    with app.test_client() as client:
+        yield client
+
+
+
+def test_invalid_password_login(client):
+    #Test user
+    test_user = {
+        "name": "Test User",
+        "email": "test@example.com",
+        "password": "testpassword",  
+        "type_of_user": "user",
+        "id": 1,
+    }
+
+    # Test data with incorrect password
+    test_data = {
+        "email": test_user["email"],
+        "password": "incorrect_password",  # Use an incorrect password
+    }
+
+    # Send a POST request to the /login route
+    response = client.post('/login', json=test_data)
+
+    # Assertions for an invalid password
+    assert response.status_code == 401
+    data = json.loads(response.data)
+    assert "error" in data
+    assert data["error"] == "Contraseña incorrecta"
